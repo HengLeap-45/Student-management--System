00f234fab29bf51240530ca2b6b45b543fa974de
Tha.Leapheng <heng334466@gmail.com>
2026-09-03 12:47:47 +0700

    first commit

---
+++ src/Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) {
+        String [] car ;
+        String [] cars = {
+                "Volkswagen",
+                "BMW",
+                "Ford",
+                "Mastang"
+        };
+        System.out.println(cars.length);
+        int [] numone = { 10 ,20 , 30 , 40};
+        String [] cars1 = {"Good ","betters"," best"};
+        System.out.println(cars1[0]);
+        System.out.println(cars1[1]);
+        System.out.println(cars1[2]);
+       
+        
+    }
+}
---
+++ src/java.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class java {
+    public static void main(String[] args) {
+        System.out.println("====SYSTEM STUDENT MANAGEMENT SYSTEM ====");
+        System.out.println("====Add Student====");
+        System.out.println("====Show Student====");
+        System.out.println("====Search Student ====");
+        System.out.println("====Delete Student ====");
+        System.out.println("====Update Student ====");
+        System.out.println("====Exit====");
+        System.out.println("=====================");
+        System.out.println("Enter Your Choice (1-5)");
+        String [] student = {"Volvo" , "BMW" ,"ford" , "Mazda"};
+        for ( String  Student : student){
+            System.out.println(Student);
+        int [] numbers = {10,20,30,40};
+        for (int  num : numbers ){
+            System.out.println(num);
+        }
+        
+        }
+        
+        
+    }
+}
